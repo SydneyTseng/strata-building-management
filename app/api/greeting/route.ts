d408299b335488f2988d27f2e58d1087e6d91d57
WeiChie <sydney.twq@gmail.com>
2025-04-04 23:38:47 +1100

    debug4pleasepleasepleasework

--- app/api/greeting/route.ts
+++ app/api/greeting/route.ts
@@ -7,18 +7,18 @@ export async function GET() {
     const timeZone = 'Asia/Sydney';
     const currentTime = format(new Date(), 'HH', { timeZone });
 
-    const hour = parseInt(currentTime, 10);
+    const hour = parseInt(currentTime, 10) + 11;
     let greetingMessage = '';
     let taskMessage = '';
     console.log('Current time in Sydney:', currentTime);
     if (hour < 12) {
-        greetingMessage = 'Good morning!' + hour;
+        greetingMessage = 'Good morning!';
         taskMessage = 'It’s a great time to check if there are any complaints logged by residents.';
     } else if (hour < 18) {
-        greetingMessage = 'Good afternoon!' + hour;
+        greetingMessage = 'Good afternoon!';
         taskMessage = 'Check the levy status of residents or update the common area maintenance schedule.';
     } else {
-        greetingMessage = 'Good evening!' + hour;
+        greetingMessage = 'Good evening!';
         taskMessage = 'Don’t forget to log any complaints you might have received today!';
     }
 
